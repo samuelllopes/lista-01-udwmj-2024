872224716e30de0d4e60f6411e348d2816201fd7
Samuel Lopes <103187575+samuelllopes@users.noreply.github.com>
2024-02-23 22:33:48 -0300

    Add files via upload

---
+++ exc1.js
@@ -0,0 +1,9 @@
+
+const numero1 = 10;
+const numero2 = 20;
+const numero3 = 30;
+
+const soma = numero1 + numero2 + numero3;
+const media = soma / 3;
+
+console.log(`A média dos números ${numero1}, ${numero2} e ${numero3} é ${media}`);
